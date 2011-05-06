c24a212dfdee1541a8318dd8a0a7cf08a836e94c
Samuel Thurston <sam.thurston@gmail.com>
2011-05-06 16:17:01 -0500

    removed debugging from js

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,9 +12,9 @@
 	$(function(){
 
 		form = $('form.crud');
-		alert('adding event')
+		
 		$('input[name="event_name"]', form).keyup($.debounce(300, function(){
-			console.log('hi');
+			
 			slug = $('input[name="slug"]', form);
 			
 			$.post(BASE_URI + 'index.php/ajax/url_title', { title : $(this).val() }, function(new_slug){
